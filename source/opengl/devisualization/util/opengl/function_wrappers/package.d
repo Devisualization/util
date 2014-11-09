cd4151c288b5a72e75a72e68a349058e906a0cb4
rikki cattermole <pbdadmin@gmail.com>
2014-11-09 21:54:59 +1300

    Woops forgot that public import

--- source/opengl/devisualization/util/opengl/function_wrappers/package.d
+++ source/opengl/devisualization/util/opengl/function_wrappers/package.d
@@ -5,3 +5,4 @@ public import devisualization.util.opengl.function_wrappers.v_1_1;
 public import devisualization.util.opengl.function_wrappers.v_1_2;
 public import devisualization.util.opengl.function_wrappers.v_1_3;
 public import devisualization.util.opengl.function_wrappers.v_1_4;
+public import devisualization.util.opengl.function_wrappers.v_1_5;
