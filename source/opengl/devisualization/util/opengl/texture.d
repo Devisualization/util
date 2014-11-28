83b33c87fdbf7b94969ff3215f70d935588a1dc5
rikki cattermole <pbdadmin@gmail.com>
2014-11-28 15:31:32 +1300

    Make it a little more obvious of what things have yet been wrapped

--- source/opengl/devisualization/util/opengl/texture.d
+++ source/opengl/devisualization/util/opengl/texture.d
@@ -1,5 +1,5 @@
 ﻿module source.opengl.devisualization.util.opengl.texture;
-import gl = derelict.opengl3.gl3;
+import glINCOMPLETE = derelict.opengl3.gl3;
 import devisualization.util.opengl.function_wrappers;
 public import devisualization.util.opengl.function_wrappers : InternalFormat, BindTextureTarget, PixelFormat;
 import devisualization.image;
@@ -108,7 +108,7 @@ struct TextureImage {
         
         void update() {
             glTexImage2D(BindTextureTarget.Texture2D, 0, InternalFormat.RGBA, cast(uint)image_.width, cast(uint)image_.height, PixelFormat.RGBA, PixelDataType.UnsignedByte, cast(void[])ubyteRawColor(image.rgba.allPixels)[0]);
-            gl.glGenerateMipmap(BindTextureTarget.Texture2D);
+            glINCOMPLETE.glGenerateMipmap(BindTextureTarget.Texture2D);
         }
     }
 }
