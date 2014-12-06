14f70b117c362f7f5b56b7572eba5f4f7a6e0423
rikki cattermole <pbdadmin@gmail.com>
2014-12-06 15:29:51 +1300

    Adding manipulation  + exporting untested

--- source/photoshop_aco/devisualization/photoshop_aco/defs.d
+++ source/photoshop_aco/devisualization/photoshop_aco/defs.d
@@ -27,10 +27,63 @@ struct ACOPalette {
 		}
 	}
 
+	void remove(string name) {
+		import std.conv : to;
+		remove(to!dstring(name));
+	}
+
+	void remove(dstring name) {
+		Color_RGBA[] ncolors;
+		dstring[] nnames;
+
+		foreach(i, n; names) {
+			if (n == name) {
+			} else {
+				ncolors ~= colors[i];
+				nnames ~= n;
+			}
+		}
+
+		colors = ncolors;
+		names = nnames;
+	}
+
+	void opAssign(op)(Color_RGBA color, string name) {
+		import std.conv : to;
+		opAssign!"="(color, to!dstring(name));
+	}
+
+	void opAssign(op)(Color_RGBA color, dstring name) {
+		static assert(op == "=");
+		remove(name);
+		colors ~= color;
+		names ~= name;
+	}
+
 	static ACOPalette parse(ubyte[] data) {
 		import devisualization.photoshop_aco.loader;
 		ACOPalette ret;
 		ret.parseACO(data);
 		return ret;
+	}
+
+	ubyte[] exportToData() {
+		import devisualization.photoshop_aco.exporter;
+		ubyte[] ret;
+		exportData(this, ret);
+		return ret;
+	}
+}
+
+void main() {
+	import std.file : read, append, write;
+
+	ubyte[] data = cast(ubyte[])read("test/Material Palette.aco");
+	ACOPalette palette = ACOPalette.parse(data);
+
+	write("out2.txt", "");
+	foreach(color; palette.colors) {
+		import std.conv : text;
+		append("out2.txt", color.toString() ~ " = " ~ text(color.ubytes) ~ "\n");
 	}
 }
---
+++ source/photoshop_aco/devisualization/photoshop_aco/exporter.d
@@ -0,0 +1,45 @@
+﻿module devisualization.photoshop_aco.exporter;
+import devisualization.photoshop_aco.defs;
+
+void exportData(ACOPalette _, out ubyte[] data) {
+	uint offset = 0;
+	writeData(_, data, 1, offset);
+	writeData(_, data, 2, offset);
+}
+
+void addValue(ref ubyte[] data, ushort value) {
+	import std.bitmanip : nativeToBigEndian;
+	data ~= nativeToBigEndian(value);
+}
+
+void addValue(ref ubyte[] data, uint value) {
+	import std.bitmanip : nativeToBigEndian;
+	data ~= nativeToBigEndian(value);
+}
+
+void addValue(ref ubyte[] data, dstring value) {
+	ubyte[] ret;
+	foreach(c; value) {
+		import std.bitmanip : nativeToBigEndian;
+		ret ~= nativeToBigEndian(c);
+	}
+	data ~= ret;
+}
+
+void writeData(ACOPalette _, ref ubyte[] data, ushort ver, ref uint offset) {
+	data.addValue(ver);
+	data.addValue(cast(ushort)_.colors.length);
+
+	foreach(i, color; _.colors) {
+		data.addValue(cast(ushort)0);
+		data.addValue(color.r);
+		data.addValue(color.g);
+		data.addValue(color.b);
+		data.addValue(0);
+
+		if (ver == 2) {
+			data.addValue(cast(uint)_.names[i].length);
+			data.addValue(_.names[i]);
+		}
+	}
+}
