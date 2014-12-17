2f769ae2fff0348a8c84fb89668f25a1acba90f8
rikki cattermole <pbdadmin@gmail.com>
2014-12-17 21:57:42 +1300

    Moving some code over from e.g. Cmsed

---
+++ source/core/devisualization/util/core/text.d
@@ -0,0 +1,128 @@
+﻿module devisualization.util.core.text;
+import std.string : indexOf, toUpper, toLower;
+import std.traits : isSomeString;
+
+/**
+ * Replaces some text in a string.
+ * 
+ * Params:
+ * 		text			=	The old text to replace from
+ * 		oldText			=	The text to remove
+ * 		newText			=	The text to replace oldText with
+ * 		caseSensitive	=	Does 'A' and 'a' the same character? Default: true
+ * 		first			=	Stop after the first instance of oldText? Default: false
+ * 
+ * Returns:
+ * 		The text that has been replaced
+ */
+pure T replace(T)(T text, T oldText, T newText, bool caseSensitive = true, bool first = false) if (isSomeString!T) {
+	T ret;
+	T tempData;
+	bool stop;
+	foreach(dchar c; text) {
+		if (tempData.length > oldText.length && !stop) {
+			ret ~= tempData;
+			tempData.length = 0;
+		}
+		if (((oldText[0 .. tempData.length] != tempData && caseSensitive) || (oldText[0 .. tempData.length].toLower() != tempData.toLower() && !caseSensitive)) && !stop) {
+			ret ~= tempData;
+			tempData.length = 0;
+		}
+		tempData ~= c;
+		if (((tempData == oldText && caseSensitive) || (tempData.toLower() == oldText.toLower() && !caseSensitive)) && !stop) {
+			ret ~= newText;
+			tempData.length = 0;
+			stop = first;
+		}
+	}
+	if (tempData != T.init) {
+		ret ~= tempData;	
+	}
+	return ret;
+}
+
+unittest {
+	string test = "Hello World!";
+	test = test.replace("Hello", "Hi");
+	assert(test == "Hi World!");
+	
+	assert(replace("Hello World!", "o", "a") == "Hella Warld!");
+	assert(replace("Hello World!", "!", "#") == "Hello World#");
+}
+
+/**
+ * Split a string by a delimater
+ * 
+ * Params:
+ * 		text		=	The text to split from
+ * 		delimater	=	The text to split by
+ * 
+ * Returns:
+ * 		An array of text that has been split
+ */
+pure T[] split(T)(T text, T delimater) if (isSomeString!T) {
+	T[] ret;
+	ptrdiff_t i;
+	while((i = text.indexOf(delimater)) >= 0) {
+		ret ~= text[0 .. i];
+		text = text[i + delimater.length .. $];
+	}
+	if (text.length > 0) {
+		ret ~= text;	
+	}
+	return ret;
+}
+
+unittest {
+	string test = "abcd|efgh|ijkl";
+	assert(test.split("|") == ["abcd", "efgh", "ijkl"]);
+	string test2 = "abcd||efgh||ijkl";
+	assert(test2.split("||") == ["abcd", "efgh", "ijkl"]);
+}
+
+/**
+ * Multiplies a string by itself and concactenates it onto the resulting string
+ *
+ * Similar to how Python's string multiplication works.
+ *
+ * Params:
+ * 		text	=	The text to multiply
+ * 		count	=	How many times to multiply it
+ *
+ * Returns:
+ * 		The multiplied string
+ */
+pure T multiply(T)(T text, size_t count) if (isSomeString!T) {
+	alias U = typeof(cast()T.init[0]);
+	U[] ret;
+	ret.length = text.length * count;
+
+	size_t j;
+	foreach(i; 0 .. count) {
+		ret[j .. j + text.length] = text[];
+		j += text.length;
+	}
+
+	return cast(T)ret;
+}
+
+unittest {
+	assert(" ".multiply(3) == "   ");
+	assert("hello ".multiply(2) == "hello hello ");
+	assert("boo".multiply(1) == "boo");
+	assert("huh".multiply(0) == "");
+}
+
+/**
+ * Gets a string which has been indented to x spaces.
+ * Uses four spaces, not tabs.
+ *
+ * Params:
+ * 		count	=	How many times to indent it
+ *
+ * Returns:
+ * 		The indented string
+ */
+pure string getSpaceIndent(size_t count) {
+	return "    ".multiply(count);
+}
---
+++ source/core/devisualization/util/core/time.d
@@ -0,0 +1,94 @@
+﻿module devisualization.util.core.time;
+import std.datetime : SysTime, Clock;
+import core.time : Duration;
+
+/**
+ * UTC+0 time
+ */
+
+@property {
+	/**
+	 * Gets the current time without any offsets.
+	 * 
+	 * Returns:
+	 * 		The time (UNIX) without any offsets.
+	 */
+	ulong utc0Time() {
+		return utc0SysTime().toUnixTime();
+	}
+
+	/**
+	 * Gets the current time without any offsets.
+	 * 
+	 * Returns:
+	 * 		The time without any offsets.
+	 */
+	SysTime utc0SysTime() {
+		return Clock.currTime().removeOffset;
+	}
+
+	/**
+	 * Gets the time this application was compiled without any offsets.
+	 * 
+	 * Returns:
+	 * 		The time (UNIX) without any offsets.
+	 */
+	ulong utc0Compiled() {
+		return utc0CompiledSysTime().toUnixTime();
+	}
+
+	/**
+	 * Gets the time this application was compiled without any offsets.
+	 * 
+	 * Returns:
+	 * 		The time without any offsets.
+	 */
+	SysTime utc0CompiledSysTime() {
+		import devisualization.util.core.text : replace, split;
+		string timestamp = __TIMESTAMP__.replace("  ", " ");
+		string[] values = timestamp.split(" ");
+		
+		string timeo;
+		timeo ~= values[4] ~ "-";
+		timeo ~= values[1] ~ "-";
+		
+		if (values[2].length == 1)
+			timeo ~= "0";
+		timeo ~= values[2] ~ " ";
+		
+		timeo ~= values[3];
+
+		return SysTime.fromSimpleString(timeo).removeOffset;
+	}
+}
+
+/**
+ * Removes the offset on a time object
+ */
+SysTime removeOffset(SysTime time) {
+	time -= time.utcOffset;
+	return time;
+}
+
+private {
+	string utc0CompiledTimeStamp() {
+		import devisualization.util.core.text : split;
+		import std.conv : to;
+		import std.string : toUpper;
+		auto time = utc0CompiledSysTime();
+		
+		string dayOfWeek = to!string(time.dayOfWeek);
+		string month = to!string(time.month);
+		dayOfWeek = cast(char)toUpper(dayOfWeek[0]) ~ dayOfWeek[1 .. $];
+		month = cast(char)toUpper(month[0]) ~ month[1 .. $];
+		
+		string output;
+		output ~= dayOfWeek ~ ", ";
+		output ~= to!string(time.day) ~ " ";
+		output ~= month ~ " ";
+		output ~= to!string(time.year) ~ " ";
+		output ~= time.toSimpleString().split(" ")[1] ~ " GMT";
+		
+		return output;
+	}
+}
