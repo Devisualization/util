0836d3679ad069971e301e2fb4ee3e5772221cec
rikki cattermole <pbdadmin@gmail.com>
2014-12-30 15:38:21 +1300

    Fixing up LineGraphs algorithms. Completely new.

--- source/core/devisualization/util/core/linegraph.d
+++ source/core/devisualization/util/core/linegraph.d
@@ -4,21 +4,24 @@
  * A line graph.
  * Given points produces all points upon the line.
  * 
+ * Does not support decimal (float) points.
+ * 
  * Could be rather useful for Devisualization.Image filters perhaps?
  */
 struct LineGraphType(bool checkNegative, TYPE) {
 	private {
 		enum GraphFunction {
-			Arc,
-			Line
+			BezierCurve,
+			Line,
+			Point
 		}
 
 		TYPE[] coordsx;
 		TYPE[] coordsy;
 		GraphFunction[] functions;
-		float[] arcRadius;
-		TYPE[] arccoordsx;
-		TYPE[] arccoordsy;
+
+		TYPE[][] xPointBezierCurve;
+		TYPE[][] yPointBezierCurve;
 	}
 
 	/**
@@ -30,7 +33,7 @@ struct LineGraphType(bool checkNegative, TYPE) {
 	 * 		starty	=	Strating y coord
 	 */
 	this(TYPE startx = 0, TYPE starty = 0) {
-		lineTo(startx, starty);
+		pointAt(startx, starty);
 	}
 
 	/**
@@ -47,23 +50,50 @@ struct LineGraphType(bool checkNegative, TYPE) {
 	}
 
 	/**
-	 * Adds an arc shape to the line.
-	 * Can produce circles.
+	 * Adds a point line.
 	 * 
 	 * Params:
 	 * 		x	=	To position
 	 * 		y	=	To position
-	 * 		cx	=	Center of arc's circle
-	 * 		cy 	=	Center of arc's circle
 	 */
-	void arcTo(TYPE x, TYPE y, TYPE cx, TYPE cy) {
-		import std.math : PI, sqrt, pow, abs;
+	void pointAt(TYPE x, TYPE y) {
 		coordsx ~= x;
 		coordsy ~= y;
-		arccoordsx ~= cx;
-		arccoordsy ~= cy;
-		arcRadius ~= sqrt(cast(float)(cx.abs - x.abs).pow(2) + cast(float)(cy.abs - y.abs).pow(2));
-		functions ~= GraphFunction.Arc;
+		functions ~= GraphFunction.Point;
+	}
+
+	/**
+	 * Adds a bezier curve.
+	 * 
+	 * Params:
+	 * 		x		=	To position
+	 * 		y		=	To position
+	 * 		xPoints	=	Points to manipulate by
+	 * 		yPoints	=	Points to manipulate by
+	 */
+	void bezierCurveTo(TYPE x, TYPE y, TYPE[] xPoints, TYPE[] yPoints) {
+		coordsx ~= x;
+		coordsy ~= y;
+		functions ~= GraphFunction.BezierCurve;
+		xPointBezierCurve ~= xPoints;
+		yPointBezierCurve ~= yPoints;
+	}
+
+	/**
+	 * Adds a bezier curve.
+	 * 
+	 * Params:
+	 * 		x	=	To position
+	 * 		y	=	To position
+	 * 		x2	=	Point to manipulate by
+	 * 		y2	=	Point to manipulate by
+	 */
+	void bezierCurveTo(TYPE x, TYPE y, TYPE x2, TYPE y2) {
+		coordsx ~= x;
+		coordsy ~= y;
+		functions ~= GraphFunction.BezierCurve;
+		xPointBezierCurve ~= [x2];
+		yPointBezierCurve ~= [y2];
 	}
 
 	/**
@@ -74,111 +104,220 @@ struct LineGraphType(bool checkNegative, TYPE) {
 	 * 		step	=	The difference between points. Default: 1
 	 */
 	void apply(void delegate(TYPE x, TYPE y) del, size_t step = 1) {
+		void d(TYPE x, TYPE y, float w) {
+			del(x, y);
+		}
+
+		applyFunc!(checkNegative, TYPE)(this, &d, step);
+	}
+
+	/**
+	 * Get all points upon the line
+	 * Includes weighting for the point. Ranges from 0f .. 1f
+	 * 
+	 * Params:
+	 * 		del		=	Delegate to call with a point
+	 * 		step	=	The difference between points. Default: 1
+	 */
+	void apply(void delegate(TYPE x, TYPE y, float weighting) del, size_t step = 1) {
 		applyFunc!(checkNegative, TYPE)(this, del, step);
 	}
 }
 
 alias LineGraph = LineGraphType!(true, size_t);
 alias SignedLineGraph = LineGraphType!(false, ptrdiff_t);
-alias FloatingLineGraph = LineGraphType!(false, float);
-
 
 private {
-	void applyFunc(bool checkNeg, TYPE, GRAPH)(GRAPH graph, void delegate(TYPE x, TYPE y) del, size_t step = 1) {
-		import std.math : floor, ceil, sin, cos, sqrt, PI;
+	void applyFunc(bool checkNeg, TYPE, GRAPH)(GRAPH graph, void delegate(TYPE x, TYPE y, float w) del, size_t step = 1) {
+		import std.math : floor, ceil, sin, cos, sqrt, PI, abs, pow;
+		import std.algorithm : swap;
+
 		with(graph) {
 			TYPE lastx = coordsx[0];
 			TYPE lasty = coordsy[0];
-			size_t arcsCount;
-			import std.stdio;
-			
+			size_t currentBezierCurve;
+			del(lastx, lasty, 1f);
+
 			foreach(i, func; functions) {
 				TYPE x = coordsx[i];
 				TYPE y = coordsy[i];
 				
 				TYPE newx = lastx;
 				TYPE newy = lasty;
-				
+
 				if (func == GraphFunction.Line) {
-					del(lastx, lasty);
-					if (x == lastx && y == lasty) {
-					} else {
-						float steepNess;
-						steepNess = (y - lasty) / (x - lastx);
-						
-						if (newx <= x) {
-							while(newx <= x) {
-								newy = cast(TYPE)(steepNess * (newx-x) + y);
-								
-								del(newx, newy);
-								newx += step;
-							}
-						} else if (newx >= x) {
-							while(newx >= x) {
-								newy = cast(TYPE)(steepNess * (newx-x) + y);
-								del(newx, newy);
+					// http://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
+					// modified to force the start and end values to be 100% weighting
 
-								bool cond = true;
-								static if (checkNeg)
-									cond = newx >= step && newy >= step;
-
-								if (cond)
-									newx -= step;
-								else
-									break;
+				 	void plot(TYPE xx, TYPE yy, float w) {
+						// is pixel in range?
+						// algo can go outside
+						if (y > lasty && yy <= y && yy >= lasty) {
+							if (x > lastx && xx <= x && xx >= lastx) {
+								del(xx, yy, w);
 							}
 						}
 					}
-				} else if (func == GraphFunction.Arc) {
-					float radius = arcRadius[arcsCount];
-					TYPE cx = arccoordsx[arcsCount];
-					TYPE cy = arccoordsy[arcsCount];
-					
-					float deg2rad(float value) {
-						enum C = 180 / PI;
-						return value / C;
-					}
-					
-					float mystep = deg2rad(step / radius);
-					float theta = 0;
 
-					bool started = false;
-					while(theta <= deg2rad(360)) {
-						float tx = cx + radius * cos(theta);
-						float ty = cy + radius * sin(theta);
-						tx = tx < cx ? ceil(tx) : floor(tx);
-						ty = ty < cy ? ceil(ty) : floor(ty);
+					TYPE ipart(float x) { return cast(TYPE)x; } // integer part of x
+					TYPE round(float x) { return ipart(x + 0.5); }
+					float fpart(float x) { return (x < 0) ?
+						(1f - (x - x.floor)) :
+						(x - x.floor); } // fractional part of x
+					float rfpart(float x) { return 1 - fpart(x); }
 
-						bool cond = true;
-						static if (checkNeg)
-							cond = tx >= step && ty >= step;
-
-						if (cond) {
-							TYPE myx = cast(TYPE)tx;
-							TYPE myy = cast(TYPE)ty;
-							
-							if (myx == x && myy == y) {
-								if (started) {
-									lastx = x;
-									lasty = y;
-									break;
-								}
-							} else if (myx == lastx && myy == lasty) {
-								if (started) {
-									break;
-								} else {
-									started = true;
-								}
-							}
-							
-							if (started) {
-								del(cast(TYPE)myx, cast(TYPE)myy);
-							}
+					void drawLine(TYPE x0, TYPE y0, TYPE x1, TYPE y1) {
+						bool steep = abs(y1 - y0) > abs(x1 - x0);
+						if (steep) {
+							swap(x0, y0);
+							swap(x1, y1);
 						}
-						theta += mystep;
+						if (x0 > x1) {
+							swap(x0, x1);
+							swap(y0, y1);
+						}
+
+						TYPE dx = x1 - x0;
+						TYPE dy = y1 - y0;
+						float gradient = (cast(float)dy / cast(float)dx);
+
+						// handle first endpoint
+						TYPE xend = round(x0);
+						float yend = y0 + gradient * (xend - x0);
+						float xgap = rfpart(x0 + 0.5f);
+						TYPE xpxl1 = xend; // this will be used in the main loop
+						TYPE ypxl1 = ipart(yend);
+						if (steep) {
+							//plot(ypxl1, xpxl1, 1f); // doubled up
+							plot(ypxl1+1, xpxl1, fpart(yend) * xgap);
+						} else {
+							//plot(xpxl1, ypxl1, 1f); // doubled up
+							plot(xpxl1, ypxl1+1,  fpart(yend) * xgap);
+						}
+						float intery = yend + gradient; // first y-intersection for the main loop
+
+						// handle second endpoint
+						xend = round(x1);
+						yend = y1 + gradient * (xend - x1);
+						xgap = fpart(x1 + 0.5);
+						TYPE xpxl2 = xend; //this will be used in the main loop
+						TYPE ypxl2 = ipart(yend);
+						if (steep) {
+							plot(ypxl2, xpxl2, 1f);
+							plot(ypxl2+1, xpxl2,  fpart(yend) * xgap);
+						} else {
+							plot(xpxl2, ypxl2,  rfpart(yend) * xgap);
+							plot(xpxl2, ypxl2+1, 2f);
+						}
+
+						// main loop
+						foreach(x; xpxl1 + 1 .. xpxl2) {
+							if (steep) {
+								plot(ipart(intery), x, rfpart(intery));
+								plot(ipart(intery)+1, x, fpart(intery));
+							} else {
+								plot(x, ipart(intery), rfpart(intery));
+								plot(x, ipart(intery)+1, fpart(intery));
+							}
+							intery += gradient * step;
+						}
 					}
-					
-					arcsCount++;
+
+					drawLine(lastx, lasty, x, y);
+					lastx = x;
+					lasty = y;
+				} else if (func == GraphFunction.Point) {
+					del(x, y, 1f);
+					lastx = x;
+					lasty = y;
+				} else if (func == GraphFunction.BezierCurve) {
+					//heavily based upon http://www.codeproject.com/Articles/25237/Bezier-Curves-Made-Simple
+
+					TYPE[] xPoints = xPointBezierCurve[currentBezierCurve];
+					TYPE[] yPoints = yPointBezierCurve[currentBezierCurve];
+					xPoints = [lastx] ~ xPoints ~ [x];
+					yPoints = [lasty] ~ yPoints ~ [y];
+
+					currentBezierCurve++;
+
+					double Ni(int n, int i) {
+						import devisualization.util.core.math : factorial;
+
+						double ni;
+						double a1 = factorial(n);
+						double a2 = factorial(i);
+						double a3 = factorial(n - i);
+						ni =  a1/ (a2 * a3);
+						return ni;
+					}
+
+					double Bernstein(int n, int i, double t) {
+						double basis;
+						double ti; /* t^i */
+						double tni; /* (1 - t)^i */
+						  
+						/* Prevent problems with pow */
+								  
+						if (t == 0.0 && i == 0) 
+							ti = 1.0; 
+						else 
+							ti = pow(t, i);
+						  
+						if (n == i && t == 1.0) 
+							tni = 1.0; 
+						else 
+							tni = pow((1 - t), (n - i));
+						  
+						//Bernstein basis
+						basis = Ni(n, i) * ti * tni; 
+						return basis;
+					}
+
+					void Bezier2D(int cpts) {
+						int npts = xPoints.length;
+						double t;
+						double step2 = 1f / (cpts - 1);
+
+			           	// Calculate points on curve
+								  
+		              	t = 0;
+									  
+		             	for (int j = 0; j < cpts; j++) { 
+							if ((1.0 - t) < 5e-6) 
+								t = 1.0;
+								  
+							double xout = 0;
+							double yout = 0;
+
+	                  		for (int k = 0; k < npts; k++) {
+								double basis = Bernstein(npts - 1, k, t);
+								xout += basis * xPoints[k];
+								yout += basis * yPoints[k];
+							}
+
+							if (!(newx == cast(TYPE)xout && newy == cast(TYPE)yout))
+								del(cast(TYPE)xout, cast(TYPE)yout, 1f);
+							newx = cast(TYPE)xout;
+							newy = cast(TYPE)yout;
+
+							t += step2;
+						}
+					}
+
+					int xPointsS;
+					if (lastx > x)
+						xPointsS = lastx - x;
+					else
+						xPointsS = x - lastx;
+					int yPointsS;
+					if (lasty > y)
+						yPointsS = lasty - y;
+					else
+						yPointsS = y - lasty;
+					Bezier2D(xPointsS * yPointsS);
+
+					lastx = x;
+					lasty = y;
 				}
 			}
 		}
